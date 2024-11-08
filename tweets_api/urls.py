8132e61787b3c2607757a365994ccd878e52aea0
Felipe Cerqueira <felipecer06@gmail.com>
2024-11-08 17:06:26 -0300

    fazendo deploy v9

--- tweets_api/urls.py
+++ tweets_api/urls.py
@@ -3,8 +3,8 @@ from rest_framework.routers import DefaultRouter
 from .views import UserViewSet, TweetViewSet
 
 router = DefaultRouter()
-router.register(r'usuarios', UserViewSet, basename='usuario')
-router.register(r'tweets', TweetViewSet, basename='tweet')
+router.register(r'api/usuarios', UserViewSet, basename='usuario')
+router.register(r'api/tweets', TweetViewSet, basename='tweet')
 
 urlpatterns = [
     path('', include(router.urls)),
