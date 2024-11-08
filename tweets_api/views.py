d4fb64da0821506afeeb3bdca11b9775fe66432a
Felipe Cerqueira <felipecer06@gmail.com>
2024-11-08 19:10:16 -0300

    modificando views

--- tweets_api/views.py
+++ tweets_api/views.py
@@ -7,6 +7,9 @@ class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all().order_by('id')
     serializer_class = UserSerializer
 
+    def partial_update(self, request, *args, **kwargs):
+        return super().partial_update(request, *args, **kwargs)
+
 
 class TweetViewSet(viewsets.ModelViewSet):
     queryset = Tweet.objects.all().order_by('-id')
